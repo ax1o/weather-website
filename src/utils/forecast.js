944c16c4ba9f9d00199911661016030d437339ce
Siddhant Shukla <siddhantgk01@gmail.com>
2019-06-09 21:37:37 +0530

    setting up forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ else if(body.error){
 callback('unable to find location',undefined)
 }
 else { 
-    callback(undefined,body.daily.data[0].summary +' it is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.')
+    callback(undefined,body.daily.data[0].summary +' High today is '+body.daily.data[0].temperatureHigh+' with the low of '+ body.daily.data[0].temperatureLow+'. It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.')
 }
 })
 }
